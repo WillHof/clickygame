8d6d24078b8c38d523cd0191c02c68a18f3d97aa
Will <whof@bu.edu>
2019-08-10 22:19:11 -0400

    image cards look good

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './App.css';
 import Nav from "./components/nav";
-import "./components/images";
 import Images from "./components/images";
-function App() {
-  return (
-    <div>
-      <Nav />
-      <div className="jumobtron-fluid">
-        <div className="container">
-          <h1 classname="display-4 mt-5 mx-auto">Clicky Game</h1>
-          <p>Click the pictures you haven't clicked to get a point. Dont click the same one twice or you lose. </p>
+import dogs from "./dogs.json";
+class App extends React.Component {
+  state = {
+    currentscore: 0,
+    guesscheck: "Click an image to begin!",
+  };
+  render() {
+    return (
+      <div>
+        <Nav
+          currentscore={this.state.currentscore}
+          guesscheck={this.state.guesscheck}
+        />
+        <div className="jumbotron-fluid">
+          <div className="container">
+            <h1 classname="display-4 mt-5 mx-auto">Clicky Game</h1>
+            <p>Click the pictures you haven't clicked to get a point. Dont click the same one twice or you lose. </p>
+          </div>
         </div>
+        <Images
+          doggos={dogs} />
       </div>
-      <Images />
-    </div>
-
-  )
+    )
+  }
 }
 export default App;
--- src/components/images.js
+++ src/components/images.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
-function imageHandler() {
-    return (<div>
-        is this working?
-</div>)
+import "./style.css"
+function renderImages(dog, idx) {
+    return (
+        <div key={idx} className="card">
+            <div className="img-container">
+                <img
+                    src={dog.image}
+                    id={dog.id}
+                />
+            </div>
+        </div>
+    )
 }
+class imageHandler extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.doggos.map((dog, idx) => {
+                    return (renderImages(dog, idx))
+                })
+                }
+            </div>
+        )
+    }
+};
 export default imageHandler
